150907c71eac96585896d8450b25380719008607
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-05-11 12:15:37 -0700

    Add javascript and jQuery to take user sentence and return new sentence.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $(".sentence").submit(function(event) {
+    event.preventDefault();
+    let newArray = [];
+
+    //collect the user's sentence from the form
+    let userInput = $("#initial-sentence").val();
+
+    //use the split method to place each word into an array
+    let words = userInput.split(' ');
+    
+    //loop through array and get any word with 3+ characters
+    words.forEach(function(word) {
+      if(word.length >= 3) {
+        newArray.push(word);
+      };
+    });
+
+    //reverse the order of the new array that is composed of the 3+ char words
+    //join the new, backwards array back into a string and display to the user
+    let backwardsArray = newArray.reverse();
+    $(".output").text(backwardsArray.join(' ') + '.');
+
+    $(".sentence").hide();
+    $(".new-sentence").show();
+
+  });
+});
